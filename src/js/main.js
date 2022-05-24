b7fb2244eb91a67459903e438c3018898083993b
bhhoang818 <88428487+bhhoang818@users.noreply.github.com>
2022-05-24 17:59:48 +0700

    add

---
+++ src/js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    var swiper = new Swiper(".mySwiper", {
+        loop: true,
+        effect: "fade",
+        // autoplay: {
+        //     delay: 5500,
+        //     disableOnInteraction: false,
+        // },
+    });
+})
